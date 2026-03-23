c89ef4dd7f3fc80500104760dd79104c9baf160c
Rob <robpetruso.code@gmail.com>
2026-03-23 12:18:26 -0400

    wired up band selection buttons

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -190,6 +190,10 @@ private:
     std::unique_ptr<BtnAttachment>  bypassButtonAttachment,
                                     soloButtonAttachment,
                                     muteButtonAttachment;
+    
+    juce::Component::SafePointer<CompressorBandControls> safePtr {this};
+    
+    void updateAttachments();
 };
 
 struct GlobalControls : juce::Component
